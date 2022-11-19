d04a647af00f6eb8cd92fd5555cb24a52f59cee1
Joe <joe.chimienti@eventdynamic.com>
2022-11-19 07:27:22 -0500

    add provider

--- src/auth/apiService.js
+++ src/auth/apiService.js
@@ -1,40 +1,48 @@
-
 function getProxy() {
-  if (process.env.NODE_ENV === "development") return window.location.origin;
-  return window.location.origin +  "/api/?apiurl=https://api.pollofeed.com"
+  if (process.env.NODE_ENV === "development") return window.location.origin
+  return window.location.origin + "/api/?apiurl=https://api.pollofeed.com"
 }
 
 const proxyUrl = getProxy()
 
 export async function oauthLogin(provider) {
-  return fetch( proxyUrl + `/api/auth/authorize/${provider}`).then((res) => res.json())
+  return fetch(proxyUrl + `/api/auth/authorize/${provider}`).then((res) =>
+    res.json()
+  )
 }
 
-
-export async function oauthCallback(provider, {code, state}) {
+export async function oauthCallback(provider, { code, state }) {
   const options = {
     method: "POST",
-    body: JSON.stringify({code, state, provider})
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ code, state, provider }),
   }
-  return fetch( proxyUrl + `/api/auth/v2/callback`, options).then((res) => res.json())
+  return fetch(proxyUrl + `/api/auth/v2/callback/${provider}`, options).then((res) =>
+    res.json()
+  )
 }
 
-
-
 export async function resumeSession() {
-  return fetch(proxyUrl +  "/api/auth/resumeSession")
+  return fetch(proxyUrl + "/api/auth/resumeSession")
 }
 
 export async function signup(email, password) {
   const p = proxyUrl + "/api/auth/signup"
-  return fetch(p, { method: 'post',
-    body: JSON.stringify({ password: password, email }), headers: { 'content-type': 'application/json' } })
+  return fetch(p, {
+    method: "post",
+    body: JSON.stringify({ password: password, email }),
+    headers: { "content-type": "application/json" },
+  })
 }
 
 export async function login(body) {
-  return fetch(proxyUrl +  "/api/auth/login", { method: 'post', body: JSON.stringify(body), headers: { 'content-type': 'application/json' } })
+  return fetch(proxyUrl + "/api/auth/login", {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "content-type": "application/json" },
+  })
 }
 
 export async function logout() {
-  return fetch(proxyUrl +  "/api/auth/logout", { method: 'get' })
+  return fetch(proxyUrl + "/api/auth/logout", { method: "get" })
 }
