7db235742db465e226beda7b7436137b7b06dfcb
Joe <joe.chimienti@eventdynamic.com>
2022-12-22 09:12:07 -0500

    test bitcoin

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -8,9 +8,13 @@
 <script>
 import BitcoinPayment from "components/BitcoinPayment"
 import WebsocketConnectionStatus from "components/WebsocketConnectionStatus"
+import { OPEN_WEBSOCKET } from "src/auth/actions"
 export default {
   name: "TestPage",
   components: { WebsocketConnectionStatus, BitcoinPayment },
+  mounted() {
+    this.$store.dispatch(OPEN_WEBSOCKET)
+  },
 }
 </script>
 
